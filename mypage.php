1c56e09488cccb70ac1ada7318688dec8cf93055
kotsuboon <kotsuboon@gmail.com>
2020-04-17 22:36:08 +0900

    first commit

---
+++ mypage.php
@@ -0,0 +1,36 @@
+<!DOCTPE html>
+<html>
+  <head>
+    <meta charset="utf8">
+    <title>ホームページのタイトル</title>
+    <style>
+      body{
+        margin: 0 auto;
+        padding: 150px;
+        width: 25%;
+        background: #fbfbfa;
+      }
+      h1{
+        color: #545454;
+        font-size: 20px;
+      }
+      a{
+        color: #545454;
+        display: block;
+      }
+      a:hover{
+        text-decoration: none;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>マイページ</h1>
+    <section>
+      <p>
+      あなたのEmailは info@gmail.com です。<br>
+      あなたのPassは password です。
+      </p>
+      <a href="index.php">ユーザー登録画面へ</a>
+    </section>
+  </body>
+</html>
